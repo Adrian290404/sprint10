e5b9ebec66b24ab809f0f0ba8410602b62d4e6e8
Adrian290404 <adrianmg2004.29@gmail.com>
2025-02-05 18:51:27 +0100

    index5.php fixed (connection bbdd)

--- index5.php
+++ index5.php
@@ -4,19 +4,31 @@
 
     $message = "";
     $query = "SELECT * FROM rooms";
-    $res = $conn -> query($query);
+    
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $res = $stmt->get_result();
 
-    if($res -> num_rows == 0){
+    if ($res->num_rows == 0) {
         echo "Error, no data provided.";
+    } 
+    else {
+        while ($row = $res->fetch_assoc()) {
+            $available = $row["avaiable"] ? "Yes" : "No";
+            $message .= "<li><b>Room Name:</b> " . ($row["room_name"]) .
+                        "<br><b>Bed Type:</b> " . ($row["bed_type"]) .
+                        "<br><b>Room Floor:</b> " . ($row["room_floor"]) .
+                        "<br><b>Facilities:</b> " . ($row["facilities"]) .
+                        "<br><b>Price:</b> " . ($row["rate"]) .
+                        "<br><b>Available:</b> " . $available .
+                        "<br><b>Image:</b> " . ($row["image"]) . "</li><br><br>";
+        }
     }
-
-    while($row = $res -> fetch_assoc()){
-        $available = $row["avaiable"] ? "Yes" : "No";
-        $message = $message . "<li><b>Room Name:</b> " . $row["room_name"] . "<br><b>Bed Type:</b> " . $row["bed_type"] . "<br><b>Room Floor:</b> " . $row["room_floor"] . "<br><b>Facilities:</b> " . $row["facilities"] . "<br><b>Price:</b> " . $row["rate"] . "<br><b>Available:</b> " . $available . "<br><b>Image:</b> " . $row["image"] . "</li><br><br>";
-    }
-
+    
+    $stmt->close();
+    $conn->close();
 ?>
 
 <ol>
-    <?php echo $message;?>
+    <?php echo $message; ?>
 </ol>
