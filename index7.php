98ed2fa1a27b1c872805e67f90928554b8e96246
Adrian290404 <adrianmg2004.29@gmail.com>
2025-02-06 21:53:47 +0100

    index7.php fixed (connection bbdd)

--- index7.php
+++ index7.php
@@ -16,32 +16,50 @@
 </html>
 
 <?php
-
     include_once("conectionHotel.php");
 
     $text = null;
     $message = "";
+    
     if (isset($_GET["roomName"]) && !empty($_GET["roomName"])){
-        $text = ($_GET["roomName"]);
+        $text = $_GET["roomName"] . '%';
     }
 
-    if ($text != null){
-        $query = "SELECT * FROM rooms WHERE room_name LIKE '$text%'";
-        $res = $conn -> query($query);
-        if ($res -> num_rows == 0) {
-            $message = "No room found.";
-        }
-        else{
-            while ($row = $res -> fetch_assoc()) {
-                $available = $row["avaiable"] ? "Yes" : "No";
-                $message = $message . "<li><b>ID:</b> " . $row["id"] . "</li><li><b>Room Name:</b> " . $row["room_name"] . "</li><li><b>Bed Type:</b> " . $row["bed_type"] . "</li><li><b>Room Floor:</b> " . $row["room_floor"] . "</li><li><b>Facilities:</b> " . $row["facilities"] . "</li><li><b>Price:</b> " . $row["rate"] . "</li><li><b>Available:</b> " . $available . "</li><li><b>Image:</b> " . $row["image"] . "</li><br><br>";
+    if ($text !== null){
+        $query = "SELECT * FROM rooms WHERE room_name LIKE ?";
+        
+        if ($stmt = $conn->prepare($query)) {
+            $stmt->bind_param("s", $text);
+            $stmt->execute();
+            $res = $stmt->get_result();
+            
+            if ($res->num_rows == 0) {
+                $message = "No room found.";
+            } 
+            else {
+                while ($row = $res->fetch_assoc()) {
+                    $available = $row["avaiable"] ? "Yes" : "No";
+                    $message .= "<li><b>ID:</b> " . $row["id"] . "</li>"
+                              . "<li><b>Room Name:</b> " . $row["room_name"] . "</li>"
+                              . "<li><b>Bed Type:</b> " . $row["bed_type"] . "</li>"
+                              . "<li><b>Room Floor:</b> " . $row["room_floor"] . "</li>"
+                              . "<li><b>Facilities:</b> " . $row["facilities"] . "</li>"
+                              . "<li><b>Price:</b> " . $row["rate"] . "</li>"
+                              . "<li><b>Available:</b> " . $available . "</li>"
+                              . "<li><b>Image:</b> " . $row["image"] . "</li><br><br>";
+                }
             }
+            
+            $stmt->close();
+        } 
+        else {
+            $message = "Database query failed.";
         }
-    }
-    else{
+    } 
+    else {
         $message = "Please enter any text on input.";
     }
 
     echo $message;
-
+    $conn->close();
 ?>
